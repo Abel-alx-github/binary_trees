110597a34156cadb07a2e31c51538d4640ef315e
abel-alx-github <abeltibeso323@gmail.com>
2024-01-30 01:09:48 -0800

    task 4-binary_tree_is_leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ *binary_tree_is_leaf - check whether the node is leaf or not
+ *@node: node to be checked
+ *Return: 1 if the node is leaf otherwise 0
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!(node->left) && !(node->right))
+		return (1);
+
+	return (0);
+}
